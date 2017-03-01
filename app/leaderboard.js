fa9cb2556369cc8e4143129de98dafbb16cfecff
Your Name <you@example.com>
2017-03-01 10:23:50 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,1 +1,28 @@
-console.log("Hello world");
+PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    },
+    'selectedClass': function(){
+      let playerId = this._id;
+      let selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected"
+      }
+    }
+  });
+  Template.leaderboard.events({
+    'click .player': function(){
+      let playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    }
+  });
+}
+
+if(Meteor.isServer){
+  console.log("Hello server");
+}
+
